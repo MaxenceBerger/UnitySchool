b1fc029345034e63dac3d2f35ce1fe0be2c36384
Maxence <maxence.berger41@gmail.com>
2022-03-22 15:55:14 +0100

    Init commit

---
+++ Assets/PlayerBehaviour.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _playerTransform.position += new Vector3(-0.1f, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _playerTransform.position += new Vector3(0.1f, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            _playerTransform.position += new Vector3(0, 0.1f, 0);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            _playerTransform.position += new Vector3(0, -0.1f, 0);
+        }
+    }
+}
