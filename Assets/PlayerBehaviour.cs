6d285f38b336647fdea48ce0fcd51d9a8559eac0
Maxence <maxence.berger41@gmail.com>
2022-03-26 12:06:42 +0100

    Create Player animation

--- Assets/PlayerBehaviour.cs
+++ Assets/PlayerBehaviour.cs
@@ -8,26 +8,36 @@ public class PlayerBehaviour : MonoBehaviour
 {
     [SerializeField]
     private Rigidbody2D _playerRB;
+
     [SerializeField]
     private float _movementForce = 100f;
+
     [SerializeField]
     private float _jumpForce = 100f;
+
     [SerializeField]
     private int _totalRemainingJumps = 2;
 
-// [SerializeField]
-// private Canvas _canvasDead;
+    [SerializeField]
+    private GameObject _canvasPlayerKilled;
+
+    [SerializeField] 
+    private Animator _animator;
 
     private PlayerState _currentState;
+    
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
 
     private int _currentRemainingJumps;
-    public GameObject gameOverUI;
 
     // Start is called before the first frame update
     private void Start()
     {
         Debug.Log("Hello world!");
         _currentRemainingJumps = _totalRemainingJumps;
+        _canvasPlayerKilled.GetComponent<Canvas>();
+        _canvasPlayerKilled.SetActive(false);
     }
 
     private void OnEnable()
@@ -42,6 +52,8 @@ public class PlayerBehaviour : MonoBehaviour
         float horizontalMovement = 0;
         float verticalMovement = 0;
 
+        _animator.SetFloat("Speed", Mathf.Abs(horizontalMovement));
+
         #region Input saut
         if (Input.GetKeyDown(KeyCode.UpArrow) /* && (_currentState == PlayerState.IsGrounded) */
             && (_currentRemainingJumps > 0))
@@ -49,6 +61,8 @@ public class PlayerBehaviour : MonoBehaviour
             verticalMovement += _jumpForce;
             _currentState = PlayerState.IsJumping;
             _currentRemainingJumps--;
+            _animator.SetFloat("Jump", Mathf.Abs(verticalMovement));
+            Debug.Log(Mathf.Abs(verticalMovement));
         }
         #endregion
         #region Inputs verticaux
@@ -57,11 +71,16 @@ public class PlayerBehaviour : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             horizontalMovement -= _movementForce;
+            Flip(horizontalMovement);
+            _animator.SetFloat("Speed", Mathf.Abs(horizontalMovement));
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             horizontalMovement += _movementForce;
+            Flip(horizontalMovement);
+            _animator.SetFloat("Speed", Mathf.Abs(horizontalMovement));
         }
+
         #endregion
 
         //Définir directement le y de la vélocité override ce que Unity calcule avec la gravité.
@@ -77,17 +96,31 @@ public class PlayerBehaviour : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // Debug.Log("Collision!");
+        float resetVerticalMovement = 0;
 
         if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
         {
             Debug.Log("Player touche le sol");
             _currentState = PlayerState.IsGrounded;
             _currentRemainingJumps = _totalRemainingJumps;
+            _animator.SetFloat("Jump", resetVerticalMovement);
         }
         if (collision.gameObject.layer == LayerMask.NameToLayer("GroundKillPlayer"))
         {
             Debug.Log("Mort");
             gameObject.SetActive(false);
+            _canvasPlayerKilled.GetComponent<Canvas>();
+            _canvasPlayerKilled.SetActive(true);
+        }
+    }
+    private void Flip(float _velocityPlayer)
+    {
+        if (_velocityPlayer > 0.1f)
+        {
+            spriteRenderer.flipX = false;
+        } else if (_velocityPlayer < -0.1f)
+        {
+            spriteRenderer.flipX = true;
         }
     }
 
